aa77acdc0f089e6b70de36136a0b74fef57ed980
nachunrui <ncr0119@163.com>
2021-04-15 00:03:52 +0800

    完成游戏初始化，飞船创建以及移动功能

---
+++ settings.py
@@ -0,0 +1,10 @@
+# Author: nachunrui
+# CreateTime: 2021/4/14 22:36
+class Settings:
+    def __init__(self):
+        """初始化游戏的设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
